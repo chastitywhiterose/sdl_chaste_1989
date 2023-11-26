1179172c731d0449ea20848c759f0aa730cb210f
Chastity White Rose <chastitywhiterose@gmail.com>
2023-11-25 23:38:48 -0600

    Add files via upload

--- parts/sdl_image/main.c
+++ parts/sdl_image/main.c
@@ -1,1 +1,73 @@
-efe
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+int width=1280,height=720;
+int loop=1;
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Event e;
+
+/* Draw a Gimpish background pattern to show transparency in the image */
+static void draw_background(SDL_Renderer *renderer, int w, int h)
+{
+    SDL_Color col[2] = {
+        { 0x66, 0x66, 0x66, 0xff },
+        { 0x99, 0x99, 0x99, 0xff },
+    };
+    int i, x, y;
+    SDL_Rect rect;
+    const int dx = 8, dy = 8;
+
+    rect.w = dx;
+    rect.h = dy;
+    for (y = 0; y < h; y += dy) {
+        for (x = 0; x < w; x += dx) {
+            /* use an 8x8 checkerboard pattern */
+            i = (((x ^ y) >> 3) & 1);
+            SDL_SetRenderDrawColor(renderer, col[i].r, col[i].g, col[i].b, col[i].a);
+
+            rect.x = x;
+            rect.y = y;
+            SDL_RenderFillRect(renderer, &rect);
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+ if(SDL_Init(SDL_INIT_VIDEO))
+ {
+  printf( "SDL could not initialize! SDL_Error: %s\n",SDL_GetError());return -1;
+ }
+ window=SDL_CreateWindow("SDL Program",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_SHOWN );
+ if(window==NULL){printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );return -1;}
+ renderer = SDL_CreateRenderer(window,-1,0);
+ if(renderer==NULL){printf( "Renderer could not be created! SDL_Error: %s\n", SDL_GetError() );return -1;}
+
+ SDL_SetRenderDrawColor(renderer,0,255,0,255);
+ SDL_RenderFillRect(renderer,NULL);
+
+ printf("SDL Program Compiled Correctly\n");
+
+ /*Draw a background pattern in case the image has transparency*/
+ draw_background(renderer, width, height);
+
+ SDL_RenderPresent(renderer);
+
+ while(loop)
+ {
+  while(SDL_PollEvent(&e))
+  {
+   if(e.type == SDL_QUIT){loop=0;}
+   if(e.type == SDL_KEYUP)
+   {
+    if(e.key.keysym.sym==SDLK_ESCAPE){loop=0;}
+   }
+  }
+ }
+
+ SDL_DestroyRenderer(renderer);
+ SDL_DestroyWindow(window);
+ SDL_Quit();
+ return 0;
+}
